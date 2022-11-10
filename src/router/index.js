d1775d5739011e344adf6129d66dc6368dc3f9cc
gabriel <gabrielcmoris@gmail.com>
2022-11-10 09:57:05 +0100

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
+    name: "home",
     path: "/",
     component: Home,
   },
   {
+    name: "/about",
     path: "/about",
     component: About,
+  },
+  {
+    name: "manage",
+    //Other solution for the redirect
+    // alias: "/manage",
+    path: "/manage-music",
+    component: Manage,
+  },
+  {
+    path: "/manage",
+    redirect: { name: "manage" },
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
 });
 
 export default router;
