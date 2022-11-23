32c21a4b61e009457df748a1edf0799bcaf2e843
gabriel <gabrielcmoris@gmail.com>
2022-11-23 11:09:30 +0100

    manifest, service worker and caching to become it a PWA

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -16,6 +16,11 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+db.enablePersistence().catch((e) => {
+  console.log("Firebase persistance error ", e.code);
+});
+
 const usersCollection = db.collection("users");
 const songsCollection = db.collection("songs");
 const commentsCollection = db.collection("comments");
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+// https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements.html#web-app-manifest
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Your Music",
+        theme_color: "#ff5e3a",
+        icons: [
+          {
+            src: "assets/img/pwa-192x192.png",
+            size: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,ico,}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
