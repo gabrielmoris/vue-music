2ec657a7b7f126705daf546a8407ae922bc3fb6b
gabriel <gabrielcmoris@gmail.com>
2022-12-09 10:38:55 +0100

    test classes

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -1,9 +1,12 @@
 import SongItem from "@/components/SongItem.vue";
 //A stub is a mook function, vitest comes with some of the Vue stubs
 import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+import { expect } from "vitest";
 
 describe("SongItem.vue", () => {
   test("render song.display_name", () => {
+    // I put this not outside because each test ca change the component and affect the next tests
+    // so it would be better to mount the component each time
     const song = {
       display_name: "Mocked test",
     };
@@ -11,7 +14,6 @@ describe("SongItem.vue", () => {
       propsData: { song },
       global: { components: { "router-link": RouterLinkStub } },
     });
-
     const compositionAuthor = wrapper.find(".text-gray-500");
     expect(compositionAuthor.text()).toBe(song.display_name);
 
@@ -20,5 +22,20 @@ describe("SongItem.vue", () => {
 
     //This shows better error logs
     // expect(compositionAuthor.text()).toBe("false test"); //expected 'Mocked test' to be 'false test'
+  });
+
+  test("render song.docID in id attribute", () => {
+    // I mount again the component
+    const song = {
+      docID: "exampletest",
+    };
+    const wrapper = shallowMount(SongItem, {
+      propsData: { song },
+      global: { components: { "router-link": RouterLinkStub } },
+    });
+    //if the target component has :id={"Mocked id"}
+    // expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
+    //if the target element has dynamic :class / static class
+    expect(wrapper.classes()).toContain(`song-id-${song.docID}`);
   });
 });
